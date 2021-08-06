8a1aaadab27071526db5ca502543a2bb0973ec2e
Ally Rilling <ally.rilling@gmail.com>
2021-08-06 11:52:41 -0500

    Initial Commit

---
+++ Stardust/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Stardust
+//
+//  Created by Ally Rilling on 8/6/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Stardust/StardustApp.swift
@@ -0,0 +1,17 @@
+//
+//  StardustApp.swift
+//  Stardust
+//
+//  Created by Ally Rilling on 8/6/21.
+//
+
+import SwiftUI
+
+@main
+struct StardustApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
